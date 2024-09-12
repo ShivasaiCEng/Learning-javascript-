050226e0be229e5251d7face2ea7936d26b16eea
ShivasaiCEng <163626577+ShivasaiCEng@users.noreply.github.com>
2024-09-12 22:37:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@
 let name=prompt("enter name:");
 let age=prompt("enter age");
 alert(name +" "+"is"+" "+age+" "+"years old.");
+//alert(`${name} is ${age} years old.`);// //only using template literals//
