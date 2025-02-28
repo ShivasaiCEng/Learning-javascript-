c689bd98fff32de3ff42d086e8bdcd3cf3bc82b2
ShivasaiCEng <163626577+ShivasaiCEng@users.noreply.github.com>
2025-02-28 17:47:08 +0530

    Add files via upload

---
+++ Dom Events/DomEvents.js
@@ -0,0 +1,7 @@
+let box=document.getElementsByClassName("box");
+let isgrabbing=false;
+let offsetx,offsety;
+box.addEventListener("mousedown",(e)=>{
+isgrabbing=true;
+offsetx=client(x)-offsetx
+});
